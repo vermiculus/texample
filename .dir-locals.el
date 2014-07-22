00221f2febb063f0e3205335c06fe091bedf59bf
Sean Allred <allred.sean@gmail.com>
2014-07-21 20:02:57 -0400

    Add directory-local variables

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (TeX-command-default . "arara")
+  (TeX-PDF-mode . t))
+ ((latex-mode
+  (TeX-PDF-mode . t))))
